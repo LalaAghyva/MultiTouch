05d9c79a9174657f66aa346db3ddcee1f92d22d3
Lala <laleagayeva78@gmail.com>
2024-02-29 22:23:20 +0400

    Initial Commit

---
+++ MultiTouch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MultiTouch
+//
+//  Created by Lala on 29.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
